bec5fc9175f6e34c7bac456302b30f89ded12d99
Aman Mishra <batmanmishra23@gmail.com>
2026-07-18 09:50:21 +0530

    done and dusted- fir se

--- backend/settings.py
+++ backend/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=settings.db_url.get_secret_value(),
-        conn_max_age=600,
+        conn_max_age=60,
         ssl_require=True
     )
 }
